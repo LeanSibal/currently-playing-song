1b8f7c79cf5d585ce1b16f5046e9eb61999d643d
Lean Sibal <leansibal@gmail.com>
2017-10-13 05:58:05 +0800

    song playlist function

--- js/script.js
+++ js/script.js
@@ -1,28 +1,103 @@
-jQuery( document ).ready(function($){
-    var fetchSongs = function() {
-        var day = moment.utc().format("dddd");
-        $.getJSON('https://radiostreaming.dk/radioapp/app/?action=playlists&radio_id=34&station=default', 
-            function( songs ){
-                console.log( songs );
-                if( songs.length < 1 ) return false;
-                var currentTime = 0;
-                var currentSong = null;
-                var playingLast = null;
-                for( i in songs ) {
-                    var date = moment( songs[i].date + ' ' + songs[i].time + ' +01', 'YYMMDD HH:mm +-HH').valueOf();
-                    if( date > currentTime ){
-                        currentTime = date;
-                        playingLast = currentSong;
-                        currentSong = songs[i];
-                    }
-                }
-                if( typeof currentSong.title !== 'undefined' && typeof currentSong.artist !== 'undefined' ) {
-                    $("#currently_playing").html( currentSong.artist + ": " + currentSong.title );
-                    $("#last_played").html( playingLast.artist + ": " + playingLast.title );
-                }
+moment.tz.add(["Europe/Copenhagen|CET CEST|-10 -20|0101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010|-2azC0 Tz0 VuO0 60q0 WM0 1fA0 1cM0 1cM0 1cM0 S00 1HA0 Nc0 1C00 Dc0 1Nc0 Ao0 1h5A0 1a00 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1cM0 1fA0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00 11A0 1qM0 WM0 1qM0 WM0 1qM0 WM0 1qM0 11A0 1o00 11A0 1o00|12e5"]);
+var allSongs = [];
+function fetchSongsByDate( day, time, callback ) {
+    jQuery.getJSON('https://radiostreaming.dk/radioapp/app/?action=playlists&radio_id=34&station=default&day=' + day + '&time=' + time , callback );
+}
+function fetchItunesAlbumArt( artist, title, song, callback ) {
+        jQuery.getJSON('http://itunes.apple.com/search?entity=song&limit=1&term=' + artist, 
+            function( albumart ) {
+                callback( song, albumart );
             }
         );
-    };
-    fetchSongs();
-    setTimeout(fetchSongs, 30000);
+}
+var playlist = [];
+function addSongToPlaylist( song ) {
+    playlist.push( song );
+    playlist.sort( function( a, b ) {
+        var left = new Date( 2017, 01, 01, a.time.substr( 0, 2 ), a.time.substr( 3, 2 ) ),
+            right = new Date( 2017, 01, 01, b.time.substr( 0, 2 ), b.time.substr( 3, 2 ) );
+        if( left < right ) return -1;
+        if( left > right ) return 1;
+        return 0;
+    });
+    layoutSongTable( playlist );
+}
+
+function layoutSongTable( playlist ) {
+    const tableRow = ({ albumArt, artist, title, time }) => `
+        <tr>
+            <td>
+                <img src="${albumArt}"/>
+            </td>
+            <td>
+                <span>${time}</span>
+            </td>
+            <td>
+                <span>${artist}</span>
+            </td>
+            <td>
+                <span>${title}</span>
+            </td>
+        </tr>
+    `;
+    jQuery('#songs-playlist').html( playlist.map( tableRow ).join('') );
+}
+jQuery( document ).ready(function($){
+    if( jQuery('.playlist').length > 0 ) {
+        jQuery('#songDate').val( moment.tz("Europe/Copenhagen").format('dddd') );
+        jQuery('#songTime').val( moment.tz("Europe/Copenhagen").format('H') );
+        var updatePlaylist = function() {
+            playlist = [];
+            var day = jQuery('#songDate').val();
+            var hour = jQuery('#songTime').val();
+            fetchSongsByDate( day, hour, function( songs ) {
+                for( var i in songs ) {
+                    var artist = songs[i].artist;
+                    var title = songs[i].title;
+                    fetchItunesAlbumArt( artist, title, songs[i], function( song, albumart ) {
+                        if( albumart.resultCount > 0 ) {
+                            addSongToPlaylist({
+                                artist: song.artist,
+                                title: song.title,
+                                time: song.time,
+                                albumArt: albumart.results[0].artworkUrl100
+                            });
+                        }
+                    } );
+                }
+            });
+        }
+        updatePlaylist();
+        jQuery('#songDate,#songTime').change( updatePlaylist );
+    }
+
+
+    if( $('#currently_playing').length > 0 ) {
+        var fetchSongs = function() {
+            var day = moment.utc().format("dddd");
+            $.getJSON('https://radiostreaming.dk/radioapp/app/?action=playlists&radio_id=34&station=default', 
+                function( songs ){
+                    console.log( songs );
+                    if( songs.length < 1 ) return false;
+                    var currentTime = 0;
+                    var currentSong = null;
+                    var playingLast = null;
+                    for( i in songs ) {
+                        var date = moment( songs[i].date + ' ' + songs[i].time + ' +01', 'YYMMDD HH:mm +-HH').valueOf();
+                        if( date > currentTime ){
+                            currentTime = date;
+                            playingLast = currentSong;
+                            currentSong = songs[i];
+                        }
+                    }
+                    if( typeof currentSong.title !== 'undefined' && typeof currentSong.artist !== 'undefined' ) {
+                        $("#currently_playing").html( currentSong.artist + ": " + currentSong.title );
+                        $("#last_played").html( playingLast.artist + ": " + playingLast.title );
+                    }
+                }
+            );
+        };
+        fetchSongs();
+        setTimeout(fetchSongs, 30000);
+    }
 });
--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Currently Playing Song Shortcode
  * PluginURI: http://www.renesejling.dk
- * Description: Shows the currently playing song by using the shortcode [currently_playing_song]
+ * Description: Shows the currently playing song by using the shortcode [currently_playing_song] and [song_playlist]
  * Author: René Sejling
  * Version: 0.0.1
  **/
@@ -22,10 +22,64 @@ function currently_playing_song_shortcode( $atts ) {
     return ob_get_clean();
 }
 
+
+add_shortcode( 'songs_playlist', 'songs_playlist_shortcode' );
+function songs_playlist_shortcode( $atts ) {
+    wp_enqueue_script( 'currently_playing_song_script' );
+    ob_start();
+?>
+<div class="playlist">
+    <div class="buttons">
+        <select id="songDate">
+            <option value="Sunday">Søndag</option>
+            <option value="Monday">Mandag</option>
+            <option value="Tuesday">Tirsdag</option>
+            <option value="Wednesday">Onsdag</option>
+            <option value="Thursday">Torsdag</option>
+            <option value="Friday">Fredag</option>
+            <option value="Saturday">Lørdag</option>
+        </select>
+        <select id="songTime">
+            <option value="1">kl. 00-01</option>
+            <option value="2">kl. 01-02</option>
+            <option value="3">kl. 02-03</option>
+            <option value="4">kl. 03-04</option>
+            <option value="5">kl. 04-05</option>
+            <option value="6">kl. 05-06</option>
+            <option value="7">kl. 06-07</option>
+            <option value="8">kl. 07-08</option>
+            <option value="9">kl. 08-09</option>
+            <option value="10">kl. 10-11</option>
+            <option value="11">kl. 11-12</option>
+            <option value="12">kl. 12-13</option>
+            <option value="13">kl. 13-14</option>
+            <option value="14">kl. 14-15</option>
+            <option value="15">kl. 15-16</option>
+            <option value="16">kl. 16-17</option>
+            <option value="17">kl. 17-18</option>
+            <option value="18">kl. 18-19</option>
+            <option value="19">kl. 19-20</option>
+            <option value="20">kl. 20-21</option>
+            <option value="21">kl. 21-22</option>
+            <option value="22">kl. 22-23</option>
+            <option value="23">kl. 23-24</option>
+            <option value="24">kl. 24-00</option>
+        </select>
+    </div>
+    <div >
+        <table id="songs-playlist">
+        </table>
+    </div>
+</div>
+<?php
+    return ob_get_clean();
+}
+
 add_action('wp_enqueue_scripts', 'enqueue_currently_playing_songs_scripts');
 function enqueue_currently_playing_songs_scripts() {
     wp_register_script( 'momentjs', plugins_url('/currently-playing-song/js/moment.min.js' ), [], '2.18.1' );
-    wp_register_script( 'currently_playing_song_script', plugins_url('/currently-playing-song/js/script.js' ), ['momentjs', 'jquery'], '1.0.0' );
+    wp_register_script( 'moment-timezone', plugins_url('/currently-playing-song/js/moment-timezone.min.js' ), ['momentjs'], '0.5.13' );
+    wp_register_script( 'currently_playing_song_script', plugins_url('/currently-playing-song/js/script.js' ), ['momentjs', 'moment-timezone', 'jquery'], '1.0.0' );
 
     wp_register_style( 'song_styles', plugins_url('/currently-playing-song/css/styles.css') );
 }
